82d2a94b05f5077958a4ec47a170838294fbdf51
FERNANDO ANDRES VIVANCO SANTIBANEZ <fernando.vivanco03@inacapmail.cl>
2020-05-21 22:12:50 -0400

    generador qr

---
+++ YoApruebo/qrcode.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="qrcode.aspx.cs" Inherits="SuperMundoHiperMegaRed.qrcode" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <div>
+                <asp:TextBox ID="textCode" runat="server"></asp:TextBox>
+                <asp:Button ID="btnGeneraQR" runat="server" Text="Generar" OnClick="btnGeneraQR_Click" />
+                <hr />
+            </div>
+            <div>
+                <img runat="server" id="imgCtrl" />
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ YoApruebo/qrcode.aspx.designer.cs
@@ -0,0 +1,53 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace SuperMundoHiperMegaRed
+{
+
+
+    public partial class qrcode
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control textCode.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox textCode;
+
+        /// <summary>
+        /// Control btnGeneraQR.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnGeneraQR;
+
+        /// <summary>
+        /// Control imgCtrl.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlImage imgCtrl;
+    }
+}
