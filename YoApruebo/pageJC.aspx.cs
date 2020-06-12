0d3c618c1f7ed7832161abdce96cfbcd83d6f51f
JORGE RAUL CORTES BASTIAS <jorge.cortes58@inacapmail.cl>
2020-06-12 19:44:32 -0400

    Humilde pagina de pruebas uwu

---
+++ YoApruebo/pageJC.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SuperMundoHiperMegaRed
+{
+    public partial class pageJC : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        protected void btn1_Click(object sender, EventArgs e)
+        {
+
+            lbl1.Text = "Hola jeje";
+
+        }
+
+    
+
+        protected void btn2_Click(object sender, EventArgs e)
+        {
+            lbl1.Text = "Hola jeje2";
+        }
+    }
+}
