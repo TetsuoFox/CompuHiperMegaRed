bbc4466680e7df89163e5459ebbab9a5993d36e6
JORGE RAUL CORTES BASTIAS <jorge.cortes58@inacapmail.cl>
2020-06-20 20:12:32 -0400

    -Implementacion de pagina de almacenes

---
+++ YoApruebo/Almacenes/Agregar Almacenes/agregarAlmacenesPage.aspx
@@ -0,0 +1,83 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/PaginaMaestra.Master" AutoEventWireup="true" CodeBehind="agregarAlmacenesPage.aspx.cs" Inherits="SuperMundoHiperMegaRed.Almacenes.Agregar_Almacenes.agregarAlmacenesPage" %>
+
+    <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+        <h1>Gestionar Almacenes</h1>
+             <form id="form2" runat="server">
+         <div  class="card card-body">
+             <h2> Agregar Nuevo Almacen</h2>
+     <hr style="width:50%">
+        
+                    <div class="input-group lg mb-3">
+                    <div class="input-group-prepend">
+                    <span class="input-group-text" id="basic-addon1">Nombre</span>
+                    </div>
+    <asp:TextBox class="form-control" ID="txtNombreAlmacen" runat="server" placeholder="Bodega Stgo"  autopostback="true" ></asp:TextBox>
+              </div>
+
+              
+
+            <div class="row">
+                  <div class="col-sm-3">
+                        <div class="form-check">
+                         <asp:CheckBox  Text="-  Esta Activo" ID="chkActivo" runat="server" class="form-check-input"  autopostback="true" value="Esta Activo"/>
+                          </div>
+                  </div>
+                 <div class="col-sm-6"></div>
+                 <div class="col-sm-3"> 
+
+                        <asp:Button ID="agregarAlmacen" value="agregarAlmacen" class="btn btn-success btn-block" runat="server" Text="Agregar Almacen" OnClick="agregarAlmacen_Click"  />
+                         
+                 </div>
+            
+            </div>
+           
+    
+                     </div> 
+                           
+                        <br />
+          <div  class="card card-body">
+               
+            
+     <h2>Almacenes en el sistema</h2>
+          
+                              <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+                                  <ContentTemplate>
+                                      <asp:ScriptManager ID="ScriptManager1" runat="server">
+                                      </asp:ScriptManager>
+                                   
+                                       <div class="row">
+                  <div class="col-sm-8">
+                                      
+                                      <asp:GridView ID="tblAlmacenes" runat="server"  class="table">
+
+                 
+
+
+
+                                      </asp:GridView>
+                                   </div>
+                  <div class="col-sm-2">
+                                
+                      <div class="d-flex flex-column">
+                      <div class="p-2">   <asp:Button  class="btn btn-dark" ID="btnActualizaTbl" runat="server"  Text="Actualizar Tabla" OnClick="btnActualizaTbl_Click"  />
+                   </div>
+                      <div class="p-2">      <asp:CheckBox class="form-check-input" ID="chkTraeActivos" runat="server" checked ="true" />
+                          <label class="form-check-label" for="chkTraeActivos">Incluye almacenes no activos</label>
+                     </div>
+                     </div>
+                      
+                         
+                      
+                            
+                                           </div>
+                      </ContentTemplate>
+                         </asp:UpdatePanel>
+                   
+          
+                </div>
+    
+        </form>
+
+
+
+</asp:Content>
---
+++ YoApruebo/Almacenes/Agregar Almacenes/agregarAlmacenesPage.aspx.designer.cs
@@ -0,0 +1,96 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace SuperMundoHiperMegaRed.Almacenes.Agregar_Almacenes {
+    
+    
+    public partial class agregarAlmacenesPage {
+        
+        /// <summary>
+        /// Control form2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form2;
+        
+        /// <summary>
+        /// Control txtNombreAlmacen.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNombreAlmacen;
+        
+        /// <summary>
+        /// Control chkActivo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox chkActivo;
+        
+        /// <summary>
+        /// Control agregarAlmacen.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button agregarAlmacen;
+        
+        /// <summary>
+        /// Control UpdatePanel1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.UpdatePanel UpdatePanel1;
+        
+        /// <summary>
+        /// Control ScriptManager1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.ScriptManager ScriptManager1;
+        
+        /// <summary>
+        /// Control tblAlmacenes.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView tblAlmacenes;
+        
+        /// <summary>
+        /// Control btnActualizaTbl.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnActualizaTbl;
+        
+        /// <summary>
+        /// Control chkTraeActivos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox chkTraeActivos;
+    }
+}
