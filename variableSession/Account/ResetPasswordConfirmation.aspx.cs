5f9917062024fb6b5aac68c7702da5b9027b896f
victor.castro <victor.castro@TI-VC-0619>
2020-07-09 21:02:29 -0400

    Proyecto Ejemplo Variables de Session

---
+++ variableSession/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace variableSession.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ variableSession/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(variableSession.Startup))]
+namespace variableSession
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
